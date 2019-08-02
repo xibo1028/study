a2ad90972e8080cd777e4fb2355f631ad3c3f795
xibo1028 <xibo1028@163.com>
2019-08-02 23:02:55 +0800

    update

--- src/com/xibo/web/Main.java
+++ src/com/xibo/web/Main.java
@@ -6,7 +6,7 @@ package com.xibo.web;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("hello xidada");
+        System.out.println("hello xidada 习大大");
 
     }
 }
